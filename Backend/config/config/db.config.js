82edf0e4f7c023732f15b26e092dfa5f8ae5a57e
this-is-neha <nehushah12@gmail.com>
2026-03-19 21:30:09 +0545

    neha

--- Backend/config/config/db.config.js
+++ Backend/config/config/db.config.js
@@ -1,18 +1,12 @@
 
-
 require("dotenv").config();
 const mongoose = require("mongoose");
 
 const { MONGO_DB_URL, MONGO_DB_NAME } = process.env;
 
-mongoose.connect(MONGO_DB_URL, {
-  dbName: MONGO_DB_NAME,
-})
-  .then(() => {
-    console.log("Mongo db connected successfully...");
-  })
+mongoose.connect(MONGO_DB_URL, { dbName: MONGO_DB_NAME })
+  .then(() => console.log("MongoDB connected successfully..."))
   .catch((err) => {
-    console.log("Error while connecting MongoDB:", err);
+    console.error("Error connecting to MongoDB:", err.message);
     process.exit(1);
   });
-
